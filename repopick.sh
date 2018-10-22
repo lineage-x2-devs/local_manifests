129eb55e7b241711deeac8ccf5b211f42714711e
ThEMarD <themard85@gmail.com>
2018-10-21 22:16:05 -0600

    update repopick.sh
    * lots of pending changes for Pie were added to repopick
    * added checkout of system/vold (repopick topics are messed right meow for it)

--- repopick.sh
+++ repopick.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 source build/envsetup.sh
 
-### audio
-# topic:tinycompress-p
-
-### bt
-# topic:pie-bt-caf
-
 ### wifi
 # topic:wifi-p
 
@@ -27,13 +21,8 @@ source build/envsetup.sh
 ### nfc
 # 223192
 
-
 # DNM: x2: selinux permissive for 16.0 bringup
 # repopick 229413
-
-repopick --topic tinycompress-p
-
-repopick --topic pie-bt-caf
 
 repopick --topic wifi-p
 
@@ -45,7 +34,39 @@ repopick --topic pie-kernel-headers
 
 repopick --topic pie-styles
 
+repopick --topic pie-recovery-pathmap
+
+repopick --topic pie-hw-fde
+
+repopick --topic pie-mode-bits
+
+repopick --topic pie-aidl-noop
+
+repopick --topic pie-recovery-pathmap
+
+repopick --topic pie-sdk-docs
+
+repopick --topic pie-powermenu
+
+repopick --topic pie-expanded-desktop
+
+repopick --topic pie-su
+
+repopick --topic pie-privacy-guard
+
+repopick --topic pie-qcom-fm
+
+repopick --topic pie-camera2
+
+repopick --topic pie-deskclock
+
+# vold: Always use libbootloader_message from bootable/recovery namespace
+cd system/vold; git fetch https://github.com/LineageOS/android_system_vold refs/changes/17/231717/2 ; git checkout FETCH_HEAD ; cd ../..
+
+
 repopick --topic leeco-msm8996-p
+
+repopick --topic leeco-x2-p
 
 # nfc: Restore pn548 support to 1.1 HAL
 repopick 223192
