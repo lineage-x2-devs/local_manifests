8374285217a596f92e040348d6ff50f4eda1244e
Marc Bourgoin <themard85@gmail.com>
2018-11-24 15:55:25 -0700

    update repopicks
    * We can build with almost no repopicks now. :)
    * Instead of doing a repopick for the thermal and vr branches commit to
    manifest, just add them to local_manifest (will need to be removed once that commit gets
    merged)

--- repopick.sh
+++ repopick.sh
@@ -1,72 +1,15 @@
 #!/bin/bash
 source build/envsetup.sh
 
-### wifi
-# topic:wifi-p
+# added to local_manifest under dependencies
+# repopick 227748; #lineage: Enable qcom thermal/vr HALs
 
-### sepolicy
-# topic:pie-qcom-sepolicy
-
-### db
-# topic:pie-db-upgrade
-
-### kernel
-# topic:pie-kernel-headers
-
-###
-# Leeco
-###
-# topic:leeco-msm8996-p
-
-### nfc
-# 223192
-
-# DNM: x2: selinux permissive for 16.0 bringup
-# repopick 229413
-
-repopick --topic wifi-p
+# QCOM Thermal/VR
+repopick 234179; # vr: Use project pathmap
+repopick 234178; # thermal: Use project pathmap
+repopick 234181; # Remove Android.mk
 
 repopick --topic pie-qcom-sepolicy
 
-repopick --topic pie-db-upgrade
-
-repopick --topic pie-kernel-headers
-
-repopick --topic pie-styles
-
-repopick --topic pie-recovery-pathmap
-
-repopick --topic pie-hw-fde
-
-repopick --topic pie-mode-bits
-
-repopick --topic pie-aidl-noop
-
-repopick --topic pie-recovery-pathmap
-
-repopick --topic pie-sdk-docs
-
-repopick --topic pie-powermenu
-
-repopick --topic pie-expanded-desktop
-
-repopick --topic pie-su
-
-repopick --topic pie-privacy-guard
-
-repopick --topic pie-qcom-fm
-
-repopick --topic pie-camera2
-
-repopick --topic pie-deskclock
-
-# vold: Always use libbootloader_message from bootable/recovery namespace
-cd system/vold; git fetch https://github.com/LineageOS/android_system_vold refs/changes/17/231717/2 ; git checkout FETCH_HEAD ; cd ../..
-
-
 repopick --topic leeco-msm8996-p
-
 repopick --topic leeco-x2-p
-
-# nfc: Restore pn548 support to 1.1 HAL
-repopick 223192
