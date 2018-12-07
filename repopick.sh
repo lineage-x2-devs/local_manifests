3725ee6d7d74f29c5dba4fb510c1f007170cfaaa
Marc Bourgoin <themard85@gmail.com>
2018-12-07 15:44:42 -0700

    Repopick: remove thermal stuff and add zl1
    * Thermal/vr stuff has been merged

--- repopick.sh
+++ repopick.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 source build/envsetup.sh
 
-# added to local_manifest under dependencies
-# repopick 227748; #lineage: Enable qcom thermal/vr HALs
-
-# QCOM Thermal/VR
-repopick 234179; # vr: Use project pathmap
-repopick 234178; # thermal: Use project pathmap
-repopick 234181; # Remove Android.mk
-
 repopick --topic pie-qcom-sepolicy
 
 repopick --topic leeco-msm8996-p
 repopick --topic leeco-x2-p
+repopick --topic leeco-zl1-p
