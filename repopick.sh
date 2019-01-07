12c86186ccab95ac2fd14d819edd1a215cc1faae
Marc Bourgoin <themard85@gmail.com>
2019-01-07 00:14:43 -0700

    repopick: add hardware/qcom/power
    * pick the four needed commits for it, otherwise cherry-pick will fail for being out of order with each other
    * also remove the device/qcom/sepolicy repopicks as we have properly working enforcing without it

--- repopick.sh
+++ repopick.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 source build/envsetup.sh
 
-repopick --topic pie-qcom-sepolicy
+# hardware/qcom/power
+repopick 237769; # power: Add specific powerhal for msm8937
+repopick 237770; # power: msm8937: Update power profile settings
+repopick 237002; # power: Handle launch and interaction hints for perf HAL platforms
+repopick 237771; # power: Build with with BOARD_VNDK_VERSION
 
+# device/leeco/msm8996-common & zl1
 repopick --topic leeco-msm8996-p
+
+# device/leeco/x2
 repopick --topic leeco-x2-p
+
+# device/leeco/zl1
 repopick --topic leeco-zl1-p
