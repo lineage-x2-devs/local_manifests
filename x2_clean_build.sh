581eefc261296a40de0e6f47bf86f11039cc8ff8
Marc Bourgoin <themard85@gmail.com>
2020-01-14 03:31:29 -0700

    bring ccache back to life

--- x2_clean_build.sh
+++ x2_clean_build.sh
@@ -7,6 +7,7 @@
 export USE_CCACHE=1
 export CCACHE_DIR=~/.ccache
 export CCACHE_MAX_SIZE=50G
+export CCACHE_EXEC=$(which ccache)
 ccache -M $CCACHE_MAX_SIZE
 
 # we want all compiler messages in English
